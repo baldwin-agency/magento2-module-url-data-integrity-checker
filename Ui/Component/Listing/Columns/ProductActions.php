d04d4621d02f1656619837001bdfe0af0d7f1513
Dan Wallis <mrdanwallis@gmail.com>
2022-06-23 22:54:59 +0100

    Add edit link to admin grids

---
+++ Ui/Component/Listing/Columns/ProductActions.php
@@ -0,0 +1,50 @@
+<?php
+namespace Baldwin\UrlDataIntegrityChecker\Ui\Component\Listing\Columns;
+
+use Magento\Framework\UrlInterface;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+
+class ProductActions extends Column
+{
+    /** @var UrlInterface */
+    protected UrlInterface $urlBuilder;
+
+    /**
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param UrlInterface $urlBuilder
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = []
+    ) {
+        $this->urlBuilder = $urlBuilder;
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+    }
+
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as &$item) {
+                $item[$this->getData('name')]['edit'] = [
+                    'href' => $this->urlBuilder->getUrl(
+                        'catalog/product/edit',
+                        ['id' => $item['productId'], 'store' => $item['storeId']]
+                    ),
+                    'ariaLabel' => __('Edit ') . $item['sku'],
+                    'label' => __('Edit'),
+                    'hidden' => false,
+                ];
+            }
+        }
+
+        return $dataSource;
+    }
+}

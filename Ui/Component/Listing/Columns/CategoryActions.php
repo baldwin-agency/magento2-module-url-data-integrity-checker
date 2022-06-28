ebf5a7d7d318d7e48f823afc827e55c4048ced69
Pieter Hoste <hoste.pieter@gmail.com>
2022-06-28 23:00:08 +0200

    Fixed some failing tests

--- Ui/Component/Listing/Columns/CategoryActions.php
+++ Ui/Component/Listing/Columns/CategoryActions.php
@@ -1,22 +1,22 @@
 <?php
+
+declare(strict_types=1);
+
 namespace Baldwin\UrlDataIntegrityChecker\Ui\Component\Listing\Columns;
 
 use Magento\Framework\UrlInterface;
 use Magento\Framework\View\Element\UiComponent\ContextInterface;
 use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Framework\View\Element\UiComponentInterface;
 use Magento\Ui\Component\Listing\Columns\Column;
 
 class CategoryActions extends Column
 {
-    /** @var UrlInterface */
-    protected UrlInterface $urlBuilder;
+    private $urlBuilder;
 
     /**
-     * @param ContextInterface $context
-     * @param UiComponentFactory $uiComponentFactory
-     * @param UrlInterface $urlBuilder
-     * @param array $components
-     * @param array $data
+     * @param array<UiComponentInterface> $components
+     * @param array<mixed> $data
      */
     public function __construct(
         ContextInterface $context,
@@ -26,21 +26,53 @@ class CategoryActions extends Column
         array $data = []
     ) {
         $this->urlBuilder = $urlBuilder;
+
         parent::__construct($context, $uiComponentFactory, $components, $data);
     }
 
+    /**
+     * @param array{
+     *  data: ?array{
+     *   items: array{
+     *    array{
+     *     catId: int,
+     *     name: string,
+     *     storeId: int,
+     *     problem: string,
+     *     hash: string
+     *    }
+     *   },
+     *   totalRecords: int
+     *  }
+     * } $dataSource
+     *
+     * @return array{
+     *  data: ?array{
+     *   items: array{
+     *    array{
+     *     catId: int,
+     *     name: string,
+     *     storeId: int,
+     *     problem: string,
+     *     hash: string
+     *    }
+     *   },
+     *   totalRecords: int
+     *  }
+     * }
+     */
     public function prepareDataSource(array $dataSource)
     {
         if (isset($dataSource['data']['items'])) {
             foreach ($dataSource['data']['items'] as &$item) {
                 $item[$this->getData('name')]['edit'] = [
-                    'href' => $this->urlBuilder->getUrl(
+                    'href'      => $this->urlBuilder->getUrl(
                         'catalog/category/edit',
                         ['id' => $item['catId'], 'store' => $item['storeId']]
                     ),
                     'ariaLabel' => __('Edit ') . $item['name'],
-                    'label' => __('Edit'),
-                    'hidden' => false,
+                    'label'     => __('Edit'),
+                    'hidden'    => false,
                 ];
             }
         }

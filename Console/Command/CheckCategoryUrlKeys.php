33f3ce031ef42d20eadf38dd8bc9f320b70ec850
Pieter Hoste <hoste.pieter@gmail.com>
2026-01-11 14:44:53 +0100

    Declared compatible with symfony/console 7.x

--- Console/Command/CheckCategoryUrlKeys.php
+++ Console/Command/CheckCategoryUrlKeys.php
@@ -51,7 +51,7 @@ class CheckCategoryUrlKeys extends ConsoleCommand
         parent::configure();
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output): int
     {
         try {
             $this->appState->setAreaCode(AppArea::AREA_CRONTAB);
